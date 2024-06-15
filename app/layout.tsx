820860be69d388b7bf321f0d4d0f124589f0ed1c
Sneh Koul <snehkoul1999@gmail.com>
2024-06-15 15:44:41 -0400

    initialize dynamic in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,8 @@ import { TailwindIndicator } from '@/components/tailwind-indicator'
 import { Providers } from '@/components/providers'
 import { Header } from '@/components/header'
 import { Toaster } from '@/components/ui/sonner'
+import { DynamicContextProvider } from '@dynamic-labs/sdk-react-core'
+import { StarknetWalletConnectors } from '@dynamic-labs/starknet'
 
 export const metadata = {
   metadataBase: process.env.VERCEL_URL
@@ -38,27 +40,37 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={cn(
-          'font-sans antialiased',
-          GeistSans.variable,
-          GeistMono.variable
-        )}
+      <DynamicContextProvider
+        settings={{
+          environmentId: 'da8ca208-a4ea-41d2-ba4e-7a5465e611b2',
+
+          walletConnectors: [StarknetWalletConnectors]
+        }}
       >
-        <Toaster position="top-center" />
-        <Providers
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
+        <body
+          className={cn(
+            'font-sans antialiased',
+            GeistSans.variable,
+            GeistMono.variable
+          )}
         >
-          <div className="flex flex-col min-h-screen">
-            <Header />
-            <main className="flex flex-col flex-1 bg-muted/50">{children}</main>
-          </div>
-          <TailwindIndicator />
-        </Providers>
-      </body>
+          <Toaster position="top-center" />
+          <Providers
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <div className="flex flex-col min-h-screen">
+              <Header />
+              <main className="flex flex-col flex-1 bg-muted/50">
+                {children}
+              </main>
+            </div>
+            <TailwindIndicator />
+          </Providers>
+        </body>
+      </DynamicContextProvider>
     </html>
   )
 }
--- app/share/[id]/page.tsx
+++ app/share/[id]/page.tsx
@@ -1,56 +1,56 @@
-// import { type Metadata } from 'next'
-// import { notFound } from 'next/navigation'
+import { type Metadata } from 'next'
+import { notFound } from 'next/navigation'
 
-// import { getSharedChat } from '@/app/actions'
-// import { ChatList } from '@/components/chat-list'
-// import { AI, UIState, getUIStateFromAIState } from '@/lib/chat/actions'
-// import { formatDate } from '@/lib/utils'
+import { getSharedChat } from '@/app/actions'
+import { ChatList } from '@/components/chat-list'
+import { AI, UIState, getUIStateFromAIState } from '@/lib/chat/actions'
+import { formatDate } from '@/lib/utils'
 
-// export const runtime = 'edge'
-// export const preferredRegion = 'home'
+export const runtime = 'edge'
+export const preferredRegion = 'home'
 
-// interface SharePageProps {
-//   params: {
-//     id: string
-//   }
-// }
+interface SharePageProps {
+  params: {
+    id: string
+  }
+}
 
-// export async function generateMetadata({
-//   params
-// }: SharePageProps): Promise<Metadata> {
-//   const chat = await getSharedChat(params.id)
+export async function generateMetadata({
+  params
+}: SharePageProps): Promise<Metadata> {
+  const chat = await getSharedChat(params.id)
 
-//   return {
-//     title: chat?.title.slice(0, 50) ?? 'Chat'
-//   }
-// }
+  return {
+    title: chat?.title.slice(0, 50) ?? 'Chat'
+  }
+}
 
-// export default async function SharePage({ params }: SharePageProps) {
-//   const chat = await getSharedChat(params.id)
+export default async function SharePage({ params }: SharePageProps) {
+  const chat = await getSharedChat(params.id)
 
-//   if (!chat || !chat?.sharePath) {
-//     notFound()
-//   }
+  if (!chat || !chat?.sharePath) {
+    notFound()
+  }
 
-//   const uiState: UIState = getUIStateFromAIState(chat)
+  const uiState: UIState = getUIStateFromAIState(chat)
 
-//   return (
-//     <>
-//       <div className="flex-1 space-y-6">
-//         <div className="border-b bg-background px-4 py-6 md:px-6 md:py-8">
-//           <div className="mx-auto max-w-2xl">
-//             <div className="space-y-1 md:-mx-8">
-//               <h1 className="text-2xl font-bold">{chat.title}</h1>
-//               <div className="text-sm text-muted-foreground">
-//                 {formatDate(chat.createdAt)} · {chat.messages.length} messages
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//         <AI>
-//           <ChatList messages={uiState} isShared={true} />
-//         </AI>
-//       </div>
-//     </>
-//   )
-// }
+  return (
+    <>
+      <div className="flex-1 space-y-6">
+        <div className="border-b bg-background px-4 py-6 md:px-6 md:py-8">
+          <div className="mx-auto max-w-2xl">
+            <div className="space-y-1 md:-mx-8">
+              <h1 className="text-2xl font-bold">{chat.title}</h1>
+              <div className="text-sm text-muted-foreground">
+                {formatDate(chat.createdAt)} · {chat.messages.length} messages
+              </div>
+            </div>
+          </div>
+        </div>
+        <AI>
+          <ChatList messages={uiState} isShared={true} />
+        </AI>
+      </div>
+    </>
+  )
+}
