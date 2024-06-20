2eecfd9fb6eca6a176f64fdec0cecebeaa35b61b
Sneh Koul <snehkoul1999@gmail.com>
2024-06-20 13:30:38 -0400

    nethermind working

--- components/test.tsx
+++ components/test.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { getTransactionSimulation } from '@/lib/starknet/nethermind'
 import { Button } from './ui/button'
 import { useStarknet } from '@/lib/hooks/use-starknet'
 
@@ -7,17 +8,30 @@ export function Test() {
   const { transfer, swap } = useStarknet()
 
   return (
-    <Button
-      onClick={() => {
-        void transfer({
-          tokenName: 'Ether',
-          amount: 100000,
-          recipient:
-            '0x072afB63Cb5C2b7B2Cd4958408Ca9BDF7ef4f1eb7162E132c10143Ce311549D2'
-        })
-      }}
-    >
-      Transfer Tokens
-    </Button>
+    <>
+      <Button
+        onClick={() => {
+          void transfer({
+            tokenName: 'Ether',
+            amount: 100000,
+            recipient:
+              '0x072afB63Cb5C2b7B2Cd4958408Ca9BDF7ef4f1eb7162E132c10143Ce311549D2'
+          })
+        }}
+      >
+        Transfer Tokens
+      </Button>
+
+      <Button
+        onClick={() => {
+          void getTransactionSimulation({
+            transactionHash:
+              '0x07aeb396a19f724020684b456fe34680c60f487414185007f71c2a996966ba12'
+          })
+        }}
+      >
+        Get Transaction Simulation
+      </Button>
+    </>
   )
 }
